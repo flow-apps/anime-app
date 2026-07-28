e0c487a050bd3f1cc0d1ff8283eb29b00a9e4121
pedrohenriquebraga <pedrobraga693@gmail.com>
2026-07-28 14:41:58 -0300

    fix expo-updates

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,30 @@
 import { MainStack } from "@/components/MainStack";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { persistor, store } from "@/redux/store";
+import * as Device from "expo-device";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 export default function StackLayout() {
+  useEffect(() => {
+    async function updateApp() {
+      if (!__DEV__ && Device.isDevice) {
+        try {
+          const update = await Updates.checkForUpdateAsync();
+          if (update.isAvailable) {
+            await Updates.fetchUpdateAsync();
+            await Updates.reloadAsync();
+          }
+        } catch (error) {
+          console.error(`Error fetching latest update: ${error}`);
+        }
+      }
+    }
+
+    updateApp();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
